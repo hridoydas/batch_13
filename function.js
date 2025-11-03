1dcc0769e4c21b3f04b0626302de0b8b9c2d1c58
hridoy <hridoydasewu@gmail.com>
2025-11-03 21:29:38 +0600

    Added gitignore

--- function.js
+++
@@ -1,7 +0,0 @@
-const allData = [2, 12, "Hello", "Twelve", "this is 5th data"];
-
-function jaicchata(monmoto, arekta, aroekta) {
-  console.log(monmoto);
-}
-
-jaicchata(allData, "Bangladesh");
--- return.js
+++
@@ -1,8 +0,0 @@
-function genrateEmail() {
-  const date = Date.now();
-  const getGmailId = `ostad_user_${date}@gmail.com`;
-  return { getGmailId, date };
-}
-
-const newEmail = genrateEmail();
-// console.log(newEmail);
