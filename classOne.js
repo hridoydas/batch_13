d8ffd840d919eb8a20a72c3022e0963493a4b57c
hridoy <hridoydasewu@gmail.com>
2025-11-03 21:20:48 +0600

    added first js file

---
+++ classOne.js
@@ -0,0 +1,16 @@
+class ClassOne {
+  constructor(model, year) {
+    this.carModel = model;
+    this.invYear = year;
+  }
+
+  one(data_one) {
+    console.log(`Car Model: ${this.carModel} \nCar Invention Year: ${this.invYear} \nnew data is: ${data_one}`);
+  }
+}
+const carOne = new ClassOne("BMW", 2025);
+carOne.one("test");
+
+Employee(Mamun, 2.5, $1000, "January");
+
+// Mamun's salary for the month January is 2500
